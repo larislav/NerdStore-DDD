350e54d8b83e87736fedd7adfe63d4844ec317e3
larislav <laarissa_barros@hotmail.com>
2022-07-07 22:40:42 -0300

    Adicionando Entidades Produto e Categoria

---
+++ src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    // Interface de marcação. Utilizada para saber se uma classe é uma raiz
+    // de agregação 
+    public interface IAggregateRoot
+    {
+    }
+}
