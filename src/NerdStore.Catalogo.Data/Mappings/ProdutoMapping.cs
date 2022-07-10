98f4509fe94d353b0bed6c6b91e8e5596a3696fd
larislav <laarissa_barros@hotmail.com>
2022-07-10 15:39:20 -0300

    Adicionando persistência com repositório - EF

---
+++ src/NerdStore.Catalogo.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        //Mapear o banco de dados
+        //Mapear tudo o que tenho para tabela de produtos
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(500)");
+
+            builder.Property(c => c.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.OwnsOne(c => c.Dimensoes, cm =>
+             {
+                 //Transformando o objeto de valor 'Dimensoes'
+                 //em colunas da minha tabela 'Produto'
+                 cm.Property(c => c.Altura)
+                 .HasColumnName("Altura")
+                 .HasColumnType("int");
+
+                 cm.Property(c => c.Largura)
+                .HasColumnName("Largura")
+                .HasColumnType("int");
+
+                 cm.Property(c => c.Profundidade)
+                .HasColumnName("Profundidade")
+                .HasColumnType("int");
+             });
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -12,6 +12,15 @@ namespace NerdStore.Catalogo.Domain
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
 
+        // EF Relation
+        public ICollection<Produto> Produtos { get; private set; }
+
+        
+        protected Categoria()
+        {
+
+        }
+
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
